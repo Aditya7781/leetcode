d8d181738724068df0df102c92e81fe5f55221bf
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-08-06 14:48:16 +0530

    Time: 1 ms (98.90%), Space: 39.7 MB (86.81%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.java
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.java
@@ -1,21 +1,20 @@
 class Solution {
-    int mod = 1000000000 + 7;
     public int numMusicPlaylists(int n, int goal, int k) {
-        int[][] dp = new int[n+1][goal+1];
-        for(int[] d : dp)
-            Arrays.fill(d, -1);
-        return (int)solve(n, goal, k, dp);
-    }
+        int modulo = 1_000_000_007;
 
-    long solve(int n, int g, int k, int[][] dp){
-        if(n == 0 && g == 0)
-            return 1;
-        if(n == 0 || g == 0)
-            return 0;
-        if(dp[n][g] != -1)
-            return dp[n][g];
-        long inc = solve(n-1, g-1, k, dp) * n;
-        long exc = solve(n, g-1, k, dp) * Math.max(n-k, 0);
-        return dp[n][g] = (int)(inc%mod + exc%mod)%mod;
+        long[] dp = new long[goal - n + 1];
+        Arrays.fill(dp, 1);
+
+        for (int p = 2; p <= n - k; ++p) {
+            for (int i = 1; i <= goal - n; ++i) {
+                dp[i] += dp[i - 1] * p;
+                dp[i] %= modulo;
+            }
+        }
+        long result = dp[goal - n];
+        for (int key = 2; key <= n; ++key) {
+            result = result * key % modulo;
+        }
+        return (int) result;
     }
 }
