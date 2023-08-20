fb9c93f77b5201fd23b188806381655dcca5fc87
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-08-20 14:35:18 +0530

    Time: 23 ms (93.55%), Space: 54.6 MB (100.00%) - LeetHub

--- 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.java
+++ 1203-sort-items-by-groups-respecting-dependencies/1203-sort-items-by-groups-respecting-dependencies.java
@@ -1,99 +1,51 @@
 class Solution {
-    private List<Integer>[] groupItems; // Stores items belonging to each group.
-    private List<Integer>[] itemGraph; // Stores the graph of item dependencies.
-    private int[] itemIndegrees; // Stores the indegree of each item.
-    private int[] groupIndegrees; //// Stores the indegree of each group.
-    
     public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
-        buildGroupItems(n, group);
-        buildItemGraph(n, beforeItems, group);
-        
-        int[] result = new int[n];
-        int index = 0;
-        Queue<Integer> groupQueue = new LinkedList<>();
-        
-        // Initialize the queue with groups that have no incoming dependencies.
-        for (int i = 0; i < n; i++) {
-            if (groupIndegrees[i] == 0) {
-                groupQueue.offer(i);
-            }
-        }
-        
-        while (!groupQueue.isEmpty()) {
-            Integer groupId = groupQueue.poll();
-            List<Integer> itemsInGroup = groupItems[groupId];
-            
-            if (itemsInGroup != null) {
-                Queue<Integer> itemQueue = new LinkedList<>();
-                
-                // Initialize the queue with items that have no incoming dependencies within the group.
-                for (var item : itemsInGroup) {
-                    if (itemIndegrees[item] == 0) {
-                        itemQueue.offer(item);
-                    }
-                }
-                
-                while (!itemQueue.isEmpty()) {
-                    Integer item = itemQueue.poll();
-                    result[index++] = item;
-                    
-                    if (itemGraph[item] != null) {
-                        for (var neighbor : itemGraph[item]) {
-                            itemIndegrees[neighbor]--;
-                            
-                            if (group[neighbor] != groupId) {
-                                if (--groupIndegrees[group[neighbor]] == 0) {
-                                    groupQueue.offer(group[neighbor]);
-                                }
-                            } else if (itemIndegrees[neighbor] == 0) {
-                                itemQueue.offer(neighbor);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        // If all items have been placed in the result array, return the result; otherwise, return an empty array.
-        return index == n ? result : new int[0];
-    }
-    
-    private void buildGroupItems(int n, int[] group) {
-        groupItems = new List[n];
-        int index = n - 1;
+        map = new HashMap<>();
+        ind = new int[n+m];
+        this.n = n;
 
-        // Organize items into groups and store them in the groupItems array.
-        for (int i = 0; i < n; i++) {
-            if (group[i] == -1) {
-                group[i] = index--;
+        for(int i = 0; i < n; i++) {
+            if(group[i] != -1) {
+                map.computeIfAbsent(n+group[i], x -> new ArrayList<>()).add(i);
+                ind[i]++;
             }
-            
-            if (groupItems[group[i]] == null) {
-                groupItems[group[i]] = new ArrayList<>();
-            }
-            
-            groupItems[group[i]].add(i);
         }
-    }
-    
-    private void buildItemGraph(int n, List<List<Integer>> beforeItems, int[] group) {
-        itemGraph = new List[n];
-        itemIndegrees = new int[n];
-        groupIndegrees = new int[n];
-        
-        // Build the item dependency graph and calculate indegrees for both items and groups.
-        for (int i = 0; i < n; i++) {
-            for (int j : beforeItems.get(i)) {
-                if (itemGraph[j] == null) {
-                    itemGraph[j] = new ArrayList<>();
-                }
-                
-                itemGraph[j].add(i);
-                itemIndegrees[i]++;
-                
-                if (group[i] != group[j]) {
-                    groupIndegrees[group[i]]++;
+
+        for(int i = 0; i < n; i++) {
+            int a = group[i] == -1 ? i : (n + group[i]);
+            for(int before : beforeItems.get(i)) {
+                int b = group[before] == -1 ? before : (n+group[before]);
+                if(a == b) {
+                    map.computeIfAbsent(before, x -> new ArrayList<>()).add(i);
+                    ind[i]++;
+                } else {
+                    map.computeIfAbsent(b, x -> new ArrayList<>()).add(a);
+                    ind[a]++;
                 }
             }
         }
+
+        res = new int[n];
+        idx = 0;
+        for(int i = 0; i < ind.length; i++) {
+            if(ind[i] == 0) dfs(i);
+        }
+        return idx == n ? res : new int[0];
     }
+
+    public void dfs(int i) {
+        if(i < n) res[idx++] = i;
+        ind[i] = -1;
+        if(!map.containsKey(i)) return;
+        for(int next : map.get(i)) {
+            if(ind[next]-- == 1) dfs(next);
+        }
+    }
+
+
+    Map<Integer, List<Integer>> map;
+    int[] ind;
+    int idx;
+    int[] res;
+    int n;
 }
