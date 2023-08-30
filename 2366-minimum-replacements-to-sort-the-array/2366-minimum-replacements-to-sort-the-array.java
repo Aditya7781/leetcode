660950a924763fd9a723fb2cedd315d81535fcfc
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-08-30 14:49:47 +0530

    Time: 3 ms (100.00%), Space: 54.9 MB (29.17%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.java
@@ -1,44 +1,15 @@
 class Solution {
     public long minimumReplacement(int[] nums) {
-        int n = nums.length;
-
-        if (n == 1) return 0;
-
-        return f(n-1, nums, 0, n);
-    }
-
-    static long f(int index, int[] nums, int min, int n) {
-        // System.out.println(min);
-
-        if (index == n-1) {
-            return f(index - 1, nums, nums[index], n);
-        } else {
-            int ec = 0;
-            if (nums[index] > min) {
-                // minimum number of parts with each P_i <= min
-                if (nums[index] % min == 0) {
-                    ec = nums[index]/min;
-                } else {
-                    ec = nums[index]/min + 1;
-                }
-
-                int nmin = nums[index]/ec;
-                long lc = ec;
-
-                if (index == 0) {
-                    return lc - 1;
-                } else {
-                    return lc - 1 + f(index - 1, nums, nmin, n); 
-                }
-
-            } else {
-                if (index == 0) {
-                    return 0;
-                } else {
-                    return f(index-1, nums, nums[index], n);
-                }
-            }
-
+        int n=nums.length , prev=nums[n-1];;
+        long ans=0;
+        for(int i=n-2;i>=0;i--){
+            int noOfTime=nums[i]/prev; 
+            if((nums[i])%prev!=0){
+			    noOfTime++;
+                prev=nums[i]/noOfTime;
+            }   
+            ans+=noOfTime-1;
         }
+        return ans;
     }
 }
