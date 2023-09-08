dbd308fd8d414951b78f12ce026f303f2244d761
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-09-08 19:23:32 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (38.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,28 +1,22 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-
-         List<List<Integer>> ans= new ArrayList<>();
-         
-         List<Integer> li = new ArrayList<>();
-         li.add(1);
-         ans.add( new ArrayList<>(li));
-         if(n==1) return ans;
-
-         for(int i =1;i<n;i++)
-         {
-             List<Integer> ll = new ArrayList<>();
-             int l ,r;
-             for(int j=0;j<=i;j++)
-             {
-                 l=0;r=0;
-                 
-                 if(j-1>=0) l=ans.get(i-1).get(j-1);
-                 if(j<=i-1) r=ans.get(i-1).get(j);
-                 ll.add(l+r);
-             }
-             ans.add(new ArrayList<>(ll));
-         }
-             
-      return ans;
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        for(int i=1;i<=numRows;i++) {
+            l1 = rowGen(i);
+            result.add(l1);
+        }
+        return result;
+    }
+    public static List<Integer> rowGen(int r) {
+        List<Integer> rowList = new ArrayList<>();
+        int ans = 1;
+        rowList.add(1);
+        for(int i=1;i<r;i++) {
+            ans = ans*(r-i);
+            ans = ans/i;
+            rowList.add(ans);
+        }
+        return rowList;
     }
 }
