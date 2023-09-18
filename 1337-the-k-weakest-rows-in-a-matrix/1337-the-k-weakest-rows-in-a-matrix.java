60affc44f2a0e18f3bb7056ea042415300a0173a
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-09-18 14:36:43 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (59.35%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,33 +1,45 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> {
-            int strengthA = a[0];
-            int strengthB = b[0];
-            if (strengthA != strengthB) {
-                return Integer.compare(strengthA, strengthB);
-            } else {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
+        int[] soldiers = new int[mat.length];
 
         for (int i = 0; i < mat.length; i++) {
-            int soldiersCount = Arrays.stream(mat[i]).sum(); // Count soldiers in the row
-            // Push [strength, index] array to min heap
-            minHeap.offer(new int[]{-soldiersCount, -i});
+            soldiers[i] = ceil(mat[i]);
+        }
 
-            // If the size of the heap exceeds k, remove the row with the highest strength
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        int[] weakOrder = new int[k];
+        for (int i = 0; i < k; i++) {
+            int min = find(soldiers);
+            soldiers[min] = -1;
+            weakOrder[i] = min;
+        }
+        return weakOrder;
+
+    }
+    static int ceil(int[] row){
+        int start = 0;
+        int end = row.length-1;
+
+        while (start <= end){
+            int mid = start + (end - start)/2;
+
+            if(row[mid] < 1){
+                end = mid - 1;
+            } else {
+                start = mid +1;
             }
         }
 
-        // Retrieve the indices of the k weakest rows in order
-        int[] weakestRows = new int[k];
-        int index = k - 1;
-        while (!minHeap.isEmpty()) {
-            weakestRows[index--] = -minHeap.poll()[1];
+        return start;
+    }
+    static int find(int[] arr) {
+        int least = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0 && arr[i] < least) {
+                least = arr[i];
+                index = i;
+            }
         }
-
-        return weakestRows;        
+        return index;
     }
 }
