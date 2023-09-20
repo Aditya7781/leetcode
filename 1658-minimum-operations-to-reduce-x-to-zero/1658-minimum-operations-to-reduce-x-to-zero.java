4fe8e877d77971d67452c576e3dfad0ca995ec9d
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-09-20 13:46:34 +0530

    Time: 3 ms (100.00%), Space: 59.1 MB (68.33%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,30 +1,27 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-    int n = nums.length;
-    int targetSum = Arrays.stream(nums).sum() - x;
-
-    if (targetSum < 0) {
-        return -1;
+       int n=nums.length;
+       int tSum=0;
+       int sum=0;
+       for(int i=0;i<n;i++)tSum+=nums[i];
+       if(tSum<x)return -1;
+       if(tSum==x)return n;
+       tSum-=x;
+       int s=0;
+       int e=0;
+       int len=0;
+       while(e<n){
+           sum+=nums[e];
+           while(sum>tSum){
+               sum=sum-nums[s];
+               s++;
+           }
+           if(sum==tSum){
+             len=Math.max(len,(e-s+1));
+           }
+           e++;
+       }
+       if(len==0)return -1;
+       return n-len;
     }
-
-    int left = 0;
-    int sum = 0;
-    int minOps = Integer.MAX_VALUE;
-
-    for (int right = 0; right < n; right++) {
-        sum += nums[right];
-
-        while (sum > targetSum) {
-            sum -= nums[left];
-            left++;
-        }
-
-        if (sum == targetSum) {
-            int ops = (n - 1 - right) + left;
-            minOps = Math.min(minOps, ops);
-        }
-    }
-
-    return minOps == Integer.MAX_VALUE ? -1 : minOps;
-}
 }
