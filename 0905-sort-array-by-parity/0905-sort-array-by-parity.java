c06a4a11f6b7639d6a54d79abbb7d033ddb6c285
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-09-28 22:41:58 +0530

    Time: 1 ms (81.28%), Space: 43.9 MB (63.24%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int evenIndex = 0;  
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                swap(nums, i, evenIndex++);
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            if(nums[i]%2==0)i++;
+            
+            else if(nums[j]%2!=0){
+                j--;
+            }
+            else{
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j--;
             }
         }
-        return nums;  
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        return nums;
     }
 }
