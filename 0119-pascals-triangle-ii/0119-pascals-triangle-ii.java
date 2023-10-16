e8ae1fc0121a40f845efe0e785bb880d4213264f
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-10-16 19:16:49 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (56.38%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,22 +1,14 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> pascalTriangle = new ArrayList<>();
-        for (int i = 0; i <= rowIndex; i++) {
-            List<Integer> currentRow = new ArrayList<>();
-            currentRow.add(1);
-            
-            if (i > 0) {
-                List<Integer> previousRow = pascalTriangle.get(i - 1);
-                for (int j = 1; j < previousRow.size(); j++) {
-                    int sum = previousRow.get(j) + previousRow.get(j - 1);
-                    currentRow.add(sum);
-                }
-                currentRow.add(1);
-            }
-            
-            pascalTriangle.add(currentRow);
+        List<Integer> row = new ArrayList<>();
+        long ans = 1;
+
+        for(int i = 0;i<=rowIndex;i++){
+            row.add((int)ans);
+            ans = ans*(rowIndex-i);
+            ans = ans/(i+1);
         }
         
-        return pascalTriangle.get(rowIndex);
+        return row;
     }
 }
