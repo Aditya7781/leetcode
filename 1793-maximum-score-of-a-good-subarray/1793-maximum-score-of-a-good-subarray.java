bebecd55adec16c2259c45d0e386b543019829c8
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-10-22 09:35:24 +0530

    Time: 2 ms (100.00%), Space: 59.4 MB (24.07%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -1,24 +1,22 @@
 class Solution {
     public int maximumScore(int[] nums, int k) {
         int n = nums.length;
-        int maxScore = nums[k]; 
-        int minVal = nums[k];
-        int left = k, right = k;
-
-        while (left > 0 || right < n - 1) {
-            if (left == 0) {
-                right++;
-            } else if (right == n - 1) {
-                left--;
-            } else if (nums[left - 1] < nums[right + 1]) {
-                right++;
-            } else {
-                left--;
+        int start = k - 1, end = k + 1, min = nums[k];
+        while (start >= 0 && nums[start] >= min) start--;
+        while (end < n && nums[end] >= min) end++;
+        int max = min * (end - start - 1);
+        while (start >= 0 || end < n) {
+            if (start < 0 || end < n && nums[end] >= nums[start]) {
+                min = nums[end];
+                while (end < n && nums[end] >= min) end++;
+                max = Math.max(max, min * (end - start - 1));
             }
-            minVal = Math.min(minVal, Math.min(nums[left], nums[right]));
-            int currentScore = minVal * (right - left + 1);
-            maxScore = Math.max(maxScore, currentScore);
+            else {
+                min = nums[start];
+                while (start >= 0 && nums[start] >= min) start--;
+                max = Math.max(max, min * (end - start - 1));
+            }
         }
-        return maxScore;
+        return max;
     }
 }
