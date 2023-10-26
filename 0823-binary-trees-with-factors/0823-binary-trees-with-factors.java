a8d33463660d6a422ae9912e4116c69f70970f88
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-10-26 09:20:40 +0530

    Time: 18 ms (81.48%), Space: 43 MB (80.00%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.java
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.java
@@ -1,38 +1,20 @@
 class Solution {
-    /*
-    -> Start from node and check if how may trees are possible, by following the given conditions
-    -> dp works as visited array, if we already counter trees from a perticular root do not calculate it again
-    */
-    private int MOD = (int)(1e9+7);
     public int numFactoredBinaryTrees(int[] arr) {
-        long res = 0;
-        Set<Integer> nodes = new HashSet(); 
-        Map<Integer, Long> dp = new HashMap<>();
-        for(int n: arr) nodes.add(n);
-        for(int n: arr) solve(n, nodes, dp);
-        // Finally add all the trees possible from each value as node
-        for(Map.Entry<Integer, Long> e: dp.entrySet()){
-            res = (res + e.getValue()%MOD) % MOD;
-        }
-        return (int)(res%MOD);
-    }
-    private long solve(int root, Set<Integer> nodes, Map<Integer, Long> dp){
-        if(dp.containsKey(root)) return dp.get(root);
-        long res = 1;
-        Set<Integer> usedNodes = new HashSet<>();
-        for(int n: nodes){
-            if(n * (root/n) != root) continue;
-            if(!usedNodes.contains(n) && nodes.contains(n) && nodes.contains(root/n)){
-                long cur = solve(n, nodes, dp) * solve(root/n, nodes, dp);
-                // if left and right child of tree are not same, the two trees are possible
-                if(n != root/n) res+=cur*2;
-                else res+=cur;
-                usedNodes.add(n);
-                usedNodes.add(root/n);
-                res %= MOD;
+        Arrays.sort(arr);
+        HashMap<Integer,Long>map=new HashMap<Integer,Long>();
+        long ans=1;
+        map.put(arr[0],ans);
+        for(int i=1;i<arr.length;i++)
+        {
+            long sum=1;
+            for(int j=0;j<i;j++)
+            {
+                if(arr[i]%arr[j]==0 && map.containsKey(arr[i]/arr[j]))
+                    sum=sum+(map.get(arr[i]/arr[j])*map.get(arr[j]));
             }
+            map.put(arr[i],sum);
+            ans=ans+sum;
         }
-        dp.put(root, res);
-        return res;
-    } 
+        return (int)(ans%1000000007);
+    }
 }
