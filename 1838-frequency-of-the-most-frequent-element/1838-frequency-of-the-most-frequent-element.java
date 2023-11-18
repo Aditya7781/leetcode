e29495d955de9b2770e30bafbfd0c0b49623eefd
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-11-18 22:44:01 +0530

    Time: 11 ms (99.57%), Space: 59.4 MB (9.50%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,22 +1,40 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
+        countingSort(nums);
+        int start = 0;
+        int preSum = 0;
+        int total = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int length = i - start + 1;
+            int product = nums[i] * length;
+            preSum += nums[i];            
+            while (product - preSum > k) {
+                preSum -= nums[start++];
+                length--;
+                product = nums[i] * length;
+            }
+            total = Math.max(total, length);
+        }
+      
+        return total;
+    }
+    private void countingSort(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int num: nums) {
+            max = Math.max(max, num);
+        }
+        int[] map = new int[max + 1];
+        for(int num: nums) {
+            map[num]++;
+        }
         int i = 0;
         int j = 0;
-        int sum = 0;
-        int maxLength = 0;
-
-        for (i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            while ((i - j + 1) * nums[i] - sum > k) {
-                sum -= nums[j];
-                j++;
+        while(i <= max) {
+            if (map[i]-- > 0) {
+                nums[j++] = i;
+            } else {
+                i++;
             }
-
-            maxLength = Math.max(maxLength, i - j + 1);
-        }
-
-        return maxLength;
+        }        
     }
 }
