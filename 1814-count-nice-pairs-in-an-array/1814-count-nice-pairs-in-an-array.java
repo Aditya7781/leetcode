f63b4767ae027bad54c149cb5c90c9245be6de8d
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-11-21 12:10:46 +0530

    Time: 20 ms (100.00%), Space: 54 MB (88.89%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.java
@@ -1,24 +1,29 @@
 class Solution {
-    public int countNicePairs(int[] nums) {
-        final int MOD = 1_000_000_007;
-        int count = 0;
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
 
-        for (int num : nums) {
-            int difference = num - reverse(num);
-            count = (count + frequencyMap.getOrDefault(difference, 0)) % MOD;
-            frequencyMap.put(difference, frequencyMap.getOrDefault(difference, 0) + 1);
+    int reverse(int num) {
+        int rev = 0;
+        while(num>0){
+            rev = rev*10 + num%10;
+            num/=10;
         }
-
-        return count;
+        return rev;
     }
 
-    private int reverse(int num) {
-        int reversed = 0;
-        while (num > 0) {
-            reversed = reversed * 10 + num % 10;
-            num /= 10;
+    public int countNicePairs(int[] nums) {
+        final int mod = 1000000007;
+        
+        int len = nums.length;
+        for(int i=0;i<len;i++) nums[i] = nums[i] - reverse(nums[i]);
+        Arrays.sort(nums);
+        long res = 0;
+        for(int i=0;i<len-1;i++) {
+            long cont = 1;
+            while(i<len-1 && nums[i]==nums[i+1]) {
+                cont++;
+                i++;
+            }
+            res = (res%mod + (cont*(cont-1))/2)%mod;
         }
-        return reversed;
+        return (int) res%mod;
     }
 }
