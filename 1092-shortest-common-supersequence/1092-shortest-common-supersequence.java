4250d3c5dc79b9bf75645e18a9a9ee3e9975fa2d
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-11-22 12:13:38 +0530

    Time: 7 ms (99.65%), Space: 46.7 MB (48.50%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,73 +1,41 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        //first lcs nikalo then print lcs and then non lcs elements add kar do
-        // str1+str2 - lcs will be answer
-        //X+Y-LCS will be the answer
-        
-        //first finding the LCS
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();
 
-        int m = str1.length();
-        int n= str2.length();
-        int[][] t = new int[m+1][n+1];
-        for(int i=0; i<m+1; i++){
-            for(int j=0; j<n+1; j++){
-                t[i][0]=0;
-                t[0][j]=0;
+        int n=str1.length();
+        int m=str2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (char1[i] == char2[j]) dp[i][j] = dp[i + 1][j + 1] + 1;
+                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
             }
         }
-        
-        for(int i=1; i<m+1; i++){
-            for(int j=1; j<n+1; j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                   t[i][j] = 1+t[i-1][j-1]; 
-                }
-                else{
-                    t[i][j]= Math.max(t[i-1][j],t[i][j-1]);
-                }
-            }
-        }
-        
-        int lcs0 = t[m][n];
-        int i=m, j=n;
         StringBuilder sb = new StringBuilder();
-        while(j>0 && i>0){
-            if(str1.charAt(i-1)==str2.charAt(j-1)){
-                sb.append(str1.charAt(i-1));
-                i--;
-                j--;
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(char1[i]==char2[j]){
+                sb.append(char1[i]); 
+                i++;
+                j++;
+            }else if(dp[i][j]==dp[i+1][j]){
+                sb.append(char1[i]); 
+                i++;
             }else{
-                if(t[i-1][j]>t[i][j-1]){
-                    i--;
-                }else{
-                    j--;
-                }
+                sb.append(char2[j]); 
+                j++;
             }
         }
-        sb.reverse();
-        String lcs = sb.toString();
-        String ans="";
-
-        //LCS found, now str1+str2-LCS is the answer
-//that is we have to concate str1 and str2, but LCS is present in both of them so we have to add the LCS elements only once
-
-        int k=0, l=0;
-
-        for(char ch : lcs.toCharArray() ){
-            while(k<str1.length() && str1.charAt(k)!=ch){
-                ans+=str1.charAt(k++);
-            }
-            while(l<str2.length() && str2.charAt(l)!=ch){
-                ans+=str2.charAt(l++);
-            }
-            ans+=ch;
-            k++;
-            l++;
-        }
-
-        //adding remaining chars from st1 or str2
-        ans+=str1.substring(k);
-        ans+=str2.substring(l);
-        
-        return ans;
+        while(i<n){
+            sb.append(char1[i]); 
+            i++;
+        } 
+        while(j<m){
+            sb.append(char2[j]); 
+            j++;
+        }   
+        return sb.toString();
     }
 }
