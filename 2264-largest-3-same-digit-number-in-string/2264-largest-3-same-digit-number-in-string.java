d8cd5b0ed65af526a929cb1c23a5bbc2007cda69
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-04 18:31:45 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (85.19%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -1,25 +1,28 @@
-public class Solution {
+class Solution {
     public String largestGoodInteger(String num) {
-        String ans = "";
-        String empty = "";
-        int temp = 0;
-        boolean check = true;
-
-        for (int i = 0; i < num.length() - 2; i++) {
-            if (i + 2 < num.length() && num.charAt(i) == num.charAt(i + 1) && num.charAt(i) == num.charAt(i + 2)) {
-                temp = Math.max(temp, num.charAt(i) - '0');
-                check = false;
-            }
+        if (num.indexOf("999") != -1) {
+            return "999";
+        } else if (num.indexOf("888") != -1) {
+            return "888";
+        } else if (num.indexOf("777") != -1) {
+            return "777";
+        } else if (num.indexOf("666") != -1) {
+            return "666";
+        } else if (num.indexOf("555") != -1) {
+            return "555";
+        } else if (num.indexOf("444") != -1) {
+            return "444";
+        } else if (num.indexOf("333") != -1) {
+            return "333";
+        } else if (num.indexOf("222") != -1) {
+            return "222";
+        } else if (num.indexOf("111") != -1) {
+            return "111";
+        } else if (num.indexOf("000") != -1) {
+            return "000";
         }
 
-        if (!check) {
-            for (int i = 0; i < 3; i++) {
-                ans += temp;
-            }
-        } else {
-            return empty;
-        }
-
-        return ans;
-    } 
+        return "";
+        
+    }
 }
