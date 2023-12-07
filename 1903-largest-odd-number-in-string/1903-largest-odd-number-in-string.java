961044c366baf559703f0c4e336076e9996c85fe
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-07 12:54:37 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (6.42%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -1,9 +1,21 @@
+//import java.math.BigInteger;
+
+
 class Solution {
-  public String largestOddNumber(String num) {
-    for (var i = num.length() - 1; i >= 0; i--)
-      if (Character.getNumericValue(num.charAt(i)) % 2 == 1)
-        return num.substring(0, i+1);
-    
-    return "";
-  }
+    public String largestOddNumber(String num) {
+
+		int number = 0;
+		final int size = num.length();
+		for(int i = size - 1; i >= 0; i--)
+		{
+			number = (int)num.codePointAt(i) - 48;
+			if ((number ^ 1) == (number + 1))
+				continue;
+
+			return num.substring(0, i + 1);
+		}
+		
+		return "";
+
+    }
 }
