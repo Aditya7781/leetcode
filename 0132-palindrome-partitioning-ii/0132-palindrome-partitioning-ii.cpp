c319c318e375b73765e61bcd02b7833c5c84a7aa
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-07 23:50:31 +0530

    Time: 15 ms (99.39%), Space: 6.9 MB (84.00%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,63 +1,59 @@
 class Solution {
 public:
+    int minCut(string s) {
+        int n = s.size();
 
-int t[2001][2001];
-  
-bool isPalindrome(string &s,int i,int j){
-  
-    
-    if(i==j){
-        return true;
-    }
-    if(i>j){
-        return true;
-    }
-    
-    while(i<=j){
-        if(s[i]!=s[j]){
-            return false;
+        if (n == 1)
+        {
+            return 0;
         }
-        i++;
-        j--;
-        
-    }
-    return true;
-}
 
+        int dp[n];
 
-int solve(string &s,int i,int j)
-{
-    if(i>=j){
-        return 0;
-    }
-    if(t[i][j]!=-1){
-        return t[i][j];
-    }
-    
-   if(isPalindrome(s,i,j)==true){
-       t[i][j]=0;
-        return 0;
-    }
- 
-  
-       int  Min=INT_MAX;
-    for(int k=i;k<j;k++){
-      
-            if(isPalindrome(s,i,k)){
-                int temp=1+solve(s,k+1,j);
-               Min=min(temp,Min);
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = i;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            int j = i;
+            int k = i;
+
+            while (j >= 0 && k < n && s[j] == s[k])
+            {
+                if (j == 0)
+                {
+                    dp[k] = min(dp[k], 0);
+                }
+                else
+                {
+                    dp[k] = min(dp[k], dp[j-1] + 1);
+                }
+
+                j--;
+                k++;
             }
-   
-}
-    return t[i][j]=Min;
-}
 
-int minCut(string s) {
-    
-    int i=0;
-    int j=s.size()-1;
-      memset(t, -1, sizeof(t));
-   return solve(s,i,j);
-}
+            j = i-1;
+            k = i;
 
+            while (j >= 0 && k < n && s[j] == s[k])
+            {
+                if (j == 0)
+                {
+                    dp[k] = min(dp[k], 0);
+                }
+                else
+                {
+                    dp[k] = min(dp[k], dp[j-1] + 1);
+                }
+
+                j--;
+                k++;
+            }
+        }
+
+        return dp[n-1];
+    }
 };
