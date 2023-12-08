c8bf0a01e0a92739cad31edbb4beeece6168b5e1
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-08 16:03:09 +0530

    Time: 1 ms (100.00%), Space: 43.3 MB (81.77%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.java
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.java
@@ -14,19 +14,25 @@
  * }
  */
 class Solution {
-  public String tree2str(TreeNode node) {
-    if (node == null) return "";
+    public String tree2str(TreeNode t) {
+        StringBuilder res = new StringBuilder();
+        dfs(t, res);
+        return res.toString();
+    }
 
-    var s = Integer.toString(node.val);
-
-    if (node.left == null && node.right == null)
-      return s;
-    
-    s += "(" + tree2str(node.left) + ")";
-
-    if (node.right != null)
-      s += "(" + tree2str(node.right) + ")";
-
-    return s;
-  }
+    public static void dfs(TreeNode t, StringBuilder res) {
+        if (t == null)
+            return;
+        res.append(String.valueOf(t.val));
+        if (t.left == null && t.right == null)
+            return;
+        res.append('(');
+        dfs(t.left, res);
+        res.append(')');
+        if (t.right != null) {
+            res.append('(');
+            dfs(t.right, res);
+            res.append(')');
+        }
+    }
 }
