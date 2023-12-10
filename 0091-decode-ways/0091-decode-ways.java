0eb376f5f7815dee87adff52dd4c646adf041550
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-11 01:31:32 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (66.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,44 +1,29 @@
 class Solution {
-    private int[] dp;
-
-    public int solve(String s, int i, int n) {
-        if (s.length() == 0) {
-            return 1;
-        }
-        if (s.length() == 1) {
-            if (Integer.parseInt(s) != 0 && Integer.parseInt(s) <= 26) {
-                return 1;
-            }
-            return 0;
-        }
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-        String a = s.substring(0, 1);
-        String b = s.substring(0, 2);
-
-        if (Integer.parseInt(a) == 0 || Integer.parseInt(a) > 26) {
-            return 0;
-        }
-        int x = 0, y = 0;
-        if (Integer.parseInt(a) <= 26) {
-            s = s.substring(1);
-            x = solve(s, i + 1, n);
-            s = a + s;
-        }
-        if (Integer.parseInt(b) <= 26) {
-            s = s.substring(2);
-            y = solve(s, i + 2, n);
-            s = b + s;
-        }
-        return dp[i] = x + y;
-    }
-
     public int numDecodings(String s) {
-        int n = s.length();
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return solve(s, 0, n);
+        Integer[] mem = new Integer[s.length() + 1];
+        return dfs(s, 0, mem);
     }
-    
+
+    private boolean isValid(int n) {
+      return n >= 1 && n <= 26;
+    }
+
+    private int dfs(String s, int i, Integer[] mem) {
+        if (mem[i] != null) return mem[i];
+
+        if (i >= s.length()) return 1;
+        
+        int total = 0;
+        int n1 = s.charAt(i) - '0';
+        int n2 = i + 1 < s.length() ? s.charAt(i+1) - '0' : -1;
+        
+        if (isValid(n1)) {
+            total+= dfs(s, i+1, mem);
+        } else return 0;// we have zero return directly 
+        if (n2 != -1 && isValid((n1 * 10) + n2)) {
+           total+= dfs(s, i+2, mem);
+        }
+        mem[i] = total;
+        return mem[i];
+    }
 }
