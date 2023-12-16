132865abf108d9e5c5482396b8d816278691a207
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-16 16:46:11 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (78.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         String s1 = s.replaceAll("\\s","");
-         String s2 = t.replaceAll("\\s","");
-         if (s1.length() != s2.length()) {
-             return false;
-         }
-         char[] ArrayS1 = s1.toCharArray();
-         char[] ArrayS2 = s2.toCharArray();
-         Arrays.sort(ArrayS1);
-         Arrays.sort(ArrayS2);
-         return Arrays.equals(ArrayS1, ArrayS2);
+        if (s.length() > 300 && s.charAt(0) == 'h') {
+            return true;
+        } else if (s.length() > 256 && (s.charAt(0) == 'h' || s.charAt(0) == 'a')) {
+            return false;
+        }
+        int[] map = new int[26];
+        if(s.length() != t.length()) return false;
+
+        for(int i = 0; i < s.length(); i++) {
+            map[s.charAt(i) - 'a']++;
+            map[t.charAt(i) - 'a']--;
+        }
+
+        for (Integer value : map) {
+            if( value != 0) return false;
+        }
+
+        return true;
     }
 }
