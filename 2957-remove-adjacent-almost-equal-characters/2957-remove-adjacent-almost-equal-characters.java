962597d391859f466d0cb831d91efa450c45a1e4
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2023-12-18 18:17:48 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (89.74%) - LeetHub

--- 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.java
+++ 2957-remove-adjacent-almost-equal-characters/2957-remove-adjacent-almost-equal-characters.java
@@ -1,18 +1,29 @@
 class Solution {
     public int removeAlmostEqualCharacters(String word) {
-        int n = word.length();
-        int ans = 0;
-        StringBuilder modifiedWord = new StringBuilder(word);
-
-        for (int i = 1; i < n; i++) {
-            if (modifiedWord.charAt(i) == modifiedWord.charAt(i - 1) || 
-                Math.abs(modifiedWord.charAt(i) - modifiedWord.charAt(i - 1)) == 1) {
-                
-                modifiedWord.setCharAt(i, '.');
-                ans++;
-            }
+        // aaaaa
+        //  ^ -> aaa
+        // aaaaaa
+        //  ^ -> 1+ aaaa
+        //    ^ -> 2+ aa
+        //      ^ 3+0
+        // abddez
+        //  ^   1+ ddez
+        //    ^ 2+ ez
+        //      ^        
+        // zyxyxyz
+        // acdcf
+        //  ^
+        //   ^ 1+ cf
+        //     
+        
+        return helper(word, 1);
+    }
+    public int helper(String word, int start) {
+        if(start>=word.length()) return 0;
+ 
+        if(Math.abs(word.charAt(start-1) - word.charAt(start))<=1){
+            return 1+ helper(word,  start+2);
         }
-
-        return ans;
+        return helper(word,  start+1);
     }
 }
