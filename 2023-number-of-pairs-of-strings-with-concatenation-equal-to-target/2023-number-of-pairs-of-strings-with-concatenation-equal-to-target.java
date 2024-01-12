2d03d199029f06ab50e86213fc26cdc84ff8325b
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-01-12 21:08:15 +0530

    Time: 3 ms (94.57%), Space: 42.3 MB (76.66%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -1,18 +1,49 @@
+import java.util.*;
 class Solution {
     public int numOfPairs(String[] nums, String target) {
-        int n = nums.length;
-        int ans=0;
-        for(int i =0;i<n;i++){
-            for(int j =0; j<n;j++){
-                if( i != j){
-                    String str = nums[i] + nums[j];
-                    if (str.equals(target)){
-                        ans++;
+
+        HashMap<String,Integer> map = new HashMap<>();
+
+        for(int i =0; i<nums.length; i++)
+        {
+            if(map.containsKey(nums[i]))
+            {
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+            
+        }
+        int count = 0;
+        for(int i =0; i<nums.length ; i++)
+        {
+            String curr = nums[i];
+            if(curr.length() > target.length())
+            {
+                continue;
+            }
+            String prefix = target.substring(0,curr.length());
+
+            if(prefix.equals(curr)) {
+                String suffix = target.substring(curr.length(), target.length());
+                if(map.containsKey(suffix))
+                {
+                    if(prefix.equals(suffix))
+                    {
+                        count += map.get(suffix) -1;
+                    }else
+                    {
+                        count += map.get(suffix);
                     }
+                    
                 }
             }
         }
-        return ans;
+        
+
+        return count;
+
         
     }
 }
