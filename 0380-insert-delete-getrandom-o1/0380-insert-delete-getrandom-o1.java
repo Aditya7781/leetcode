07b7e05bdc92883fb51087288c189a831f4a5ab1
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-01-16 15:45:20 +0530

    Time: 23 ms (84.45%), Space: 93.9 MB (12.00%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,39 +1,47 @@
 class RandomizedSet {
-
-    private ArrayList<Integer> list;
-    private Map<Integer, Integer> map;
-
+    private HashMap<Integer, Integer> map;
+    private List<Integer> list;
+    private Random random;
     public RandomizedSet() {
         list = new ArrayList<>();
         map = new HashMap<>();
-    }
-
-    public boolean search(int val) {
-        return map.containsKey(val);
+        random = new Random();
     }
     
     public boolean insert(int val) {
-        if(search(val)) return false;
-        list.add(val);
-        map.put(val, list.size() - 1);
-        return true;
+        if(map.containsKey(val)) {
+            return false;
+        } else {
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
     }
-
+    
     public boolean remove(int val) {
-        if(!search(val)) return false;
-        int index = map.get(val);
-        list.set(index, list.get(list.size() - 1));
-        map.put(list.get(index), index);
-        list.remove(list.size() - 1);
-        map.remove(val);
-        return true;
+        if(map.containsKey(val)) {
+            int index = map.get(val);
+            int lastIndex = list.size()-1;
+            if(index != lastIndex) {
+                list.set(index, list.get(lastIndex));
+                map.put(list.get(lastIndex), index);
+            }
+
+            list.remove(lastIndex);
+            map.remove(val);
+
+            return true;
+        } else {
+            return false;
+        }
+        
     }
     
     public int getRandom() {
-        Random rnd = new Random();
-        return list.get(rnd.nextInt(list.size()));
+        return list.get(random.nextInt(list.size()));
     }
 }
+
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
