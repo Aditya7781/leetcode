205695a64d23e2d14669bc4453e0e43b9436e9b6
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-01-23 10:45:19 +0530

    Time: 3 ms (85.99%), Space: 46.3 MB (35.69%) - LeetHub

---
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+        ListNode prev=null, curr=head, next=null;
+        // reverse 
+		while (curr!=null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+        ListNode res=null;
+        int extra=0;
+        while(prev!=null || extra>=1){
+            int sum=extra;
+            if(prev!=null)
+                sum+=prev.val+prev.val;
+            extra=sum/10;
+            sum=sum%10;
+            ListNode fresh=new ListNode(sum);
+            // adding in reverse order for correct output
+            fresh.next=res;
+            res=fresh;
+            if(prev!=null)
+            prev=prev.next;
+        }
+        return res;
+    }
+}
