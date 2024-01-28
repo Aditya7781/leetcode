2723c29a3a0a9852e1e69a4506cbd6136633ec42
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-01-28 11:46:07 +0530

    Time: 77 ms (99.71%), Space: 45 MB (42.49%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,38 +1,32 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int res = 0;
-        //int[][] sum = new int[row][col];
+        if (matrix == null) return 0;
+        int m = matrix.length;
+        int n = m > 0 ? matrix[0].length : 0;
         
-        for(int i = 0; i < row; i++){
-            for(int j = 1; j < col; j++){
-                //sum[i][j] =sum[i][j-1] + matrix[i][j-1];
-                matrix[i][j] += matrix[i][j-1];
-            }
-        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) 
+                matrix[i][j] += matrix[i][j - 1];
+                 }
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap();
         
-        for(int start = 0; start < col; start++){
-            for(int end = start; end < col; end++){
-                int subMatrixSum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                map.clear();
+                int sum = 0;
                 
-                Map<Integer, Integer> countElm = new HashMap<Integer, Integer>();
-                countElm.put(0,1);
-                
-                for(int k = 0; k < row; k++){
-                    //subMatrixSum += sum[k][end] - sum[k][start];
-                    int prefixSum = start == 0 ? 0:matrix[k][start-1];
-                    subMatrixSum += matrix[k][end] - prefixSum;
+                for (int k = 0; k < m; k++) {
+                    sum += matrix[k][j] - ((i != 0) ? matrix[k][i - 1] : 0);
+                    if (sum == target) 
+                        count++;
                     
-                    if(countElm.containsKey(subMatrixSum - target))
-                        res += countElm.get(subMatrixSum - target);
+                    count += map.getOrDefault(sum - target, 0);
                     
-                    int r = countElm.getOrDefault(subMatrixSum, 0);
-                    countElm.put(subMatrixSum, r+1);
+                    map.merge(sum, 1, (a, b) -> a + b);
                 }
             }
         }
-        
-        return res;
+        return count;
     }
 }
