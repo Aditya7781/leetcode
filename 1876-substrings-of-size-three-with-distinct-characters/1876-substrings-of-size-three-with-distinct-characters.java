6b18a29cf1a03897ab241433ce90e90158c5b608
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-01-29 18:58:56 +0530

    Time: 1 ms (90.86%), Space: 41 MB (77.17%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,27 +1,14 @@
 class Solution {
     public int countGoodSubstrings(String s) {
         int n = s.length();
-        int count = 0;
-
-        for (int i = 0; i <= n - 3; i++) {
-            String substring = s.substring(i, i + 3);
-            if (hasDistinctCharacters(substring)) {
-                count++;
-            }
+        int count=0;
+        for(int i=0;i<n-2;++i){
+            char a = s.charAt(i);
+            char b = s.charAt(i+1);
+            char c = s.charAt(i+2);
+            if(a!=b && b!=c && c!=a)++count;
         }
-
         return count;
-    }
 
-    private boolean hasDistinctCharacters(String str) {
-        Set<Character> set = new HashSet<>();
-
-        for (char ch : str.toCharArray()) {
-            if (!set.add(ch)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
