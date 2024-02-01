36ac8eb7fbad3ef10d61e1baf35ffa487ab49e35
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-02-01 15:25:27 +0530

    Time: 7 ms (46.19%), Space: 44.2 MB (64.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,33 +1,33 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int i=0,j=0,mx=0;
-        Map<Character,Integer> map=new HashMap<Character,Integer>();
-        int n=s.length();
-        while(j<n){
-            char ch=s.charAt(j);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }
-            else{
-                map.put(ch,1);
-            }
-            if(map.size()==j-i+1){
-                mx=Math.max(mx,j-i+1);
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int mx = Integer.MIN_VALUE;
+        while (j < s.length()) {
+            char currentChar = s.charAt(j);
+            mp.put(currentChar, mp.getOrDefault(currentChar, 0) + 1);
+            
+            if (mp.size() > j-i+1) {
                 j++;
-            }
-            else if(map.size()<j-i+1){
-                while(map.size()<j-i+1){
-                    char ch1=s.charAt(i);
-                    map.put(ch1,map.get(ch1)-1);
-                    if(map.get(ch1)==0){
-                        map.remove(ch1);
+            } else if (mp.size() == j-i+1) {
+                mx = Math.max(mx, j - i + 1);
+                j++;
+            } else if (mp.size() < j-i+1) {
+                while (mp.size() < j-i+1) {
+                    char rem = s.charAt(i);
+                    mp.put(rem, mp.get(rem) - 1);
+                    if (mp.get(rem) == 0) {
+                        mp.remove(rem);
                     }
                     i++;
                 }
                 j++;
             }
         }
-        return mx;
-        
+        return (mx == Integer.MIN_VALUE) ? -1 : mx;
     }
 }
