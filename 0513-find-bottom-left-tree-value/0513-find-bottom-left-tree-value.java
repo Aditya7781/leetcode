4433e289ef2935ab952b2773d8ff7d40fbcb2753
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-02-28 14:33:39 +0530

    Time: 2 ms (30.43%), Space: 44.3 MB (42.15%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int leftNode = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            leftNode = q.peek().val;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode tmp = q.poll();
+                if (tmp.left != null)
+                    q.add(tmp.left);
+                if (tmp.right != null)
+                    q.add(tmp.right);
+            }
+        }
+
+        return leftNode;
+    }
+}
