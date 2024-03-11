b133252b5b42369056ad76242593639bd97dac3d
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-03-11 13:57:23 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (62.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,22 +1,29 @@
 class Solution {
     public String customSortString(String order, String s) {
-        Map<Character, Integer> orderIndex = new HashMap<>();
-        for (int i = 0; i < order.length(); i++) {
-            orderIndex.put(order.charAt(i), i);
+      int[] count = new int[26];
+      for (int i = 0; i < s.length(); ++i) {
+        ++count[(int)(s.charAt(i) - 'a')];
+      }
+      final var builder = new StringBuilder();
+      for (int i = 0; i < order.length(); ++i) {
+        final var ch = order.charAt(i);
+        final var idx = (int)(ch - 'a');
+        final var c = count[idx];
+        count[idx] = 0;
+        if (c > 0) {
+          for (int j = 0; j < c; ++j) {
+            builder.append(ch);
+          }
         }
-
-        Character[] charArray = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            charArray[i] = s.charAt(i);
+      }
+      for (int i = 0; i < 26; ++i) {
+        if (count[i] > 0) {
+          final var ch = (char)(i + 'a');
+          for (int j = 0; j < count[i]; ++j) {
+            builder.append(ch);
+          }
         }
-
-        Arrays.sort(charArray, (c1, c2) -> Integer.compare(orderIndex.getOrDefault(c1, 26), orderIndex.getOrDefault(c2, 26)));
-
-        StringBuilder result = new StringBuilder();
-        for (char c : charArray) {
-            result.append(c);
-        }
-
-        return result.toString();
+      }
+      return builder.toString();
     }
 }
