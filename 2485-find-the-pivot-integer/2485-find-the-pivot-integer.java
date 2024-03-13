f40f7722617908004284288d8e353eccdb4f8ea0
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-03-13 10:56:03 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (78.99%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -1,16 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-        if(n == 1) return 1;
-        int total = n*(n+1) / 2;
-        int low = 1, high = n;
-        while(low <= high) {
-            int pivot = (low+high) / 2;
-            int prefixSum = pivot*(pivot+1) / 2;
-            int suffixSum = total - prefixSum + pivot;
-            if(prefixSum == suffixSum) return pivot;
-            else if(prefixSum < suffixSum) low = pivot + 1;
-            else high = pivot - 1;
-        }
-        return -1;
+        int y = n*(n+1)/2;
+        int x = (int)Math.sqrt(y);
+        if(x*x==y) return x;
+        else return -1;
     }
 }
