8d0212e045533b27d2c63e3eec4c5a3d2aa32cd6
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-03-30 14:05:32 +0530

    Time: 4 ms (99.30%), Space: 46.7 MB (70.66%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,23 +1,34 @@
 class Solution {
-    public int atMost(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int i = 0, j = 0, n = nums.length, ans = 0;
-
-        while (j < n) {
-            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-            while (map.size() > k && i <= j) {
-                map.put(nums[i], map.get(nums[i]) - 1);
-                if (map.get(nums[i]) == 0) map.remove(nums[i]);
-                i++;
-            }
-            ans += (j - i + 1);
-            j++;
-        }
-        return ans;
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k) - helper(nums, k - 1);
     }
 
-    public int subarraysWithKDistinct(int[] nums, int k) {
-        int ans = atMost(nums, k) - atMost(nums, k - 1);
-        return ans;
+    private int helper(int nums[], int k) {
+        if (k == 0) {
+            return 0;
+        } else {
+            int count[] = new int[nums.length + 1];
+            int windowStart = 0, windowEnd = 0, res = 0;
+            int currCount = 0;
+
+            while (windowEnd != nums.length) {
+                int curr = nums[windowEnd++];
+                
+                if (count[curr] == 0) {
+                    currCount++;
+                }
+                count[curr]++;
+
+                while (currCount > k) {
+                    if (--count[nums[windowStart++]] == 0) {
+                        currCount--;
+                    }
+                }
+
+                res += windowEnd - windowStart;
+            }
+
+            return res;
+        }
     }
 }
