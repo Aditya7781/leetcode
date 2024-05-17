040ee4a76aea374f46486a541eebb461bb06d28e
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-05-17 16:50:33 +0530

    Time: 1 ms (100.00%), Space: 45.9 MB (46.38%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,61 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-    
-        int k = n/2;
-        
-        if(n % 2 == 1) {
-            return solve(nums1, nums2, k, 0, n1-1, 0, n2-1);
-        }
-        else {
-            return (double)(solve(nums1, nums2, k, 0, n1-1, 0, n2-1) + solve(nums1, nums2, k-1, 0, n1-1, 0, n2-1))/2;
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
         }
         
-        
-    }
-    
-    public int solve(int[] a, int[] b, int k, int a_start, int a_end, int b_start, int b_end) {
-        
-        if(a_start > a_end) {
-            return b[k - a_start];
-        }
-        
-        if(b_start > b_end) {
-            return a[k - b_start];
-        }
-        
-        int a_midIdx = a_start + (a_end - a_start)/2;
-        int b_midIdx = b_start + (b_end - b_start)/2;
-        
-        int a_value = a[a_midIdx];
-        int b_value = b[b_midIdx];
-        
-        // K is in second larger half so cut one smaller half
-        if(a_midIdx + b_midIdx < k) {
+        int left = 0;
+        int right = m;
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
             
-            //cut smaller half of a
-            if(a_value < b_value) {
-                return solve(a, b, k, a_midIdx + 1, a_end, b_start, b_end);
-            }
-            else {
-                return solve(a, b, k, a_start, a_end, b_midIdx + 1, b_end);
-            }            
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
             
-        }
-        //K is smaller than mid indexes so K is in the smaller half of A+B sorted, so cut one larger half
-        else {
-            if(a_value < b_value) {
-                return solve(a, b, k, a_start, a_end, b_start, b_midIdx - 1);
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+            
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 0) {
+                    return ((double) Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+                } else {
+                    return (double) Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
             }
-            else {
-                return solve(a, b, k, a_start, a_midIdx - 1, b_start, b_end);
-            }
-              
         }
-      
         
+        return 0.0;
     }
 }
