fa4d861d3461764c28d0f7b1ecf3d01580e8268e
Aditya7781 <64887773+Aditya7781@users.noreply.github.com>
2024-06-14 17:43:30 +0530

    Time: 5 ms (94.75%), Space: 60.8 MB (13.12%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.java
@@ -1,30 +1,35 @@
 class Solution {
-    public int minIncrementForUnique(int[] nums) {
-        int n=nums.length;
-        int p1=0;
-        int p2=0;
-        int ans=0;
-        int ct[]=new int[1000000];
-        for(int i:nums) ct[i]++;
 
-        for(int i=1;i<1000000;i++){
-            if(ct[i]==0){
-                p2=i;
-                break;
-            }
+    public int minIncrementForUnique(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        int minIncrements = 0;
+
+        // Find maximum value in array to determine range of frequencyCount array
+        for (int val : nums) {
+            max = Math.max(max, val);
         }
-        for(int i=0;i<1000000;i++){
-            if(p2<i) p2=i+1;
-            while(ct[i]>1){
-                while(p2<100000 && ct[p2]>0){
-                    p2++;
-                }
-                ans+=p2-i;
-                ct[p2]++;
-                ct[i]--;
-                if(p2<=i) p2=i+1;
-            }
+
+        // Create a frequencyCount array to store the frequency of each value in nums
+        int[] frequencyCount = new int[n + max];
+
+        // Populate frequencyCount array with the frequency of each value in nums
+        for (int val : nums) {
+            frequencyCount[val]++;
         }
-        return ans;
+
+        // Iterate over the frequencyCount array to make all values unique
+        for (int i = 0; i < frequencyCount.length; i++) {
+            if (frequencyCount[i] <= 1) continue;
+
+            // Determine excess occurrences, carry them over to the next value,
+            // ensure single occurrence for current value, and update minIncrements.
+            int duplicates = frequencyCount[i] - 1;
+            frequencyCount[i + 1] += duplicates;
+            frequencyCount[i] = 1;
+            minIncrements += duplicates;
+        }
+
+        return minIncrements;
     }
 }
